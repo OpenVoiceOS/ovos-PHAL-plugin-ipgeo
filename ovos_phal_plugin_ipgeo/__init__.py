8b5afb033549095fc5ec9dd0164ae6fdb362518d
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2023-06-21 16:20:01 +0100

    feat/network_reqs (#6)
    * feat/network_reqs
    
    * Refactor to `runtime_requirements`
    
    * Remove outdated docstring in `runtime_requirements` property
    
    * Remove leftover build files and add .gitignore
    
    ---------
    
    Co-authored-by: Daniel McKnight <daniel@neongecko.com>

--- ovos_phal_plugin_ipgeo/__init__.py
+++ ovos_phal_plugin_ipgeo/__init__.py
@@ -1,8 +1,11 @@
 import requests
+
 from ovos_plugin_manager.phal import PHALPlugin
 from ovos_config.config import LocalConf
 from ovos_config.locations import get_webcache_location
 from ovos_utils.messagebus import Message
+from ovos_utils import classproperty
+from ovos_utils.process_utils import RuntimeRequirements
 
 
 class IPGeoPlugin(PHALPlugin):
@@ -13,6 +16,15 @@ class IPGeoPlugin(PHALPlugin):
         self.bus.on("mycroft.internet.connected", self.on_reset)
         self.bus.on("ovos.ipgeo.update", self.on_reset)
         self.on_reset()  # get initial location data
+
+    @classproperty
+    def runtime_requirements(self):
+        return RuntimeRequirements(internet_before_load=True,
+                                   network_before_load=True,
+                                   requires_internet=True,
+                                   requires_network=True,
+                                   no_internet_fallback=False,
+                                   no_network_fallback=False)
 
     def on_reset(self, message=None):
         # we update the remote config to allow
