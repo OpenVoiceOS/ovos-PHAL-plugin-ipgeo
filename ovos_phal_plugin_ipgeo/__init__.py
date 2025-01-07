34a5e5114dc08c64886e72e09801102cc0ea4776
miro <jarbasai@mailfence.com>
2025-01-07 01:37:16 +0000

    fix:deprecations
    companion to https://github.com/OpenVoiceOS/ovos-config/pull/194

--- ovos_phal_plugin_ipgeo/__init__.py
+++ ovos_phal_plugin_ipgeo/__init__.py
@@ -1,19 +1,18 @@
 from ovos_bus_client.util import get_message_lang
-from ovos_config.config import LocalConf
+from ovos_config.config import update_mycroft_config
 from ovos_config import Configuration
-from ovos_config.locations import get_webcache_location
+from ovos_config.locations import ASSISTANT_CONFIG
 from ovos_plugin_manager.phal import PHALPlugin
 from ovos_utils import classproperty
 from ovos_utils.geolocation import get_ip_geolocation
 from ovos_utils.log import LOG
-from ovos_utils.messagebus import Message
+from ovos_bus_client.message import Message
 from ovos_utils.process_utils import RuntimeRequirements
 
 
 class IPGeoPlugin(PHALPlugin):
     def __init__(self, bus=None, config=None):
         super().__init__(bus, "ovos-phal-plugin-ipgeo", config)
-        self.web_config = LocalConf(get_webcache_location())
         self.bus.on("mycroft.internet.connected", self.on_reset)
         self.bus.on("ovos.ipgeo.update", self.on_reset)
         self.on_reset()  # get initial location data
@@ -28,22 +27,16 @@ class IPGeoPlugin(PHALPlugin):
                                    no_network_fallback=False)
 
     def on_reset(self, message=None):
-        # we update the remote config to allow
-        # both backend and user config to take precedence
-        # over ip geolocation
-        if self.web_config.get("location") and \
-                (message is None or not message.data.get('overwrite')):
-            LOG.debug("Skipping overwrite of existing location")
-            return
         # geolocate from ip address
         try:
             location = get_ip_geolocation(lang=get_message_lang(message) or Configuration().get("lang", "en"))
             if not location:
                 raise ValueError("IP geolocation returned empty location")
             LOG.info(f"IP geolocation: {location}")
-            self.web_config["location"] = location
-            self.web_config.store()
-            LOG.debug(f"Updated config: {self.web_config.path}")
+
+            update_mycroft_config(config={"location": location}, bus=self.bus)
+
+            LOG.debug(f"Updated config: {ASSISTANT_CONFIG}")
             self.bus.emit(Message("configuration.updated"))
             if message:
                 self.bus.emit(message.response(data={'location': location}))
